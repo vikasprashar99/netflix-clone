edb8b9c93975d74a487300f09a9fe93346178b32
vikasprashar99 <vikasprashar99@gmail.com>
2021-05-07 20:37:13 +0530

    homepage with bg

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import "./style.css";
+const Homepage = () => {
+    return (
+        <div className="main-container">
+                        {/* <div className="netflix-bg"> */}
 
-const Homepage=()=> {
-  return (
-   <div></div>
-  );
+            {/* </div> */}
+            {/* <img 
+            className="netflixBg"
+            alt="netflixbgimage" 
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/7a33204b-893d-4792-8494-b9d6cbc73848/8ed3a98a-e2c2-488d-9583-93634199485d/IN-en-20210502-popsignuptwoweeks-perspective_alpha_website_small.jpg" 
+            /> */}
+        </div>
+    );
 }
 
 export default Homepage;
