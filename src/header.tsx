18b05c293c53a8b2b9739a2b63ee9fca5586a957
vikasprashar99 <vikasprashar99@gmail.com>
2021-05-07 21:16:32 +0530

    header added

---
+++ src/header.tsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+    return (
+        <div className="header-main-container">
+            <div className="netflix-logo"></div>
+            <div className="signinbutton-container">
+                <a href="www.google.com" className="signin-button">Sign In</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/homepage.tsx
+++ src/homepage.tsx
@@ -1,15 +1,9 @@
 import "./style.css";
+import Header from './header';
 const Homepage = () => {
     return (
         <div className="main-container">
-                        {/* <div className="netflix-bg"> */}
-
-            {/* </div> */}
-            {/* <img 
-            className="netflixBg"
-            alt="netflixbgimage" 
-            src="https://assets.nflxext.com/ffe/siteui/vlv3/7a33204b-893d-4792-8494-b9d6cbc73848/8ed3a98a-e2c2-488d-9583-93634199485d/IN-en-20210502-popsignuptwoweeks-perspective_alpha_website_small.jpg" 
-            /> */}
+            <Header/>
         </div>
     );
 }
