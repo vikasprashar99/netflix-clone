de32209e09bae079ec596c86c7ce846786ffbc41
vikasprashar99 <vikasprashar99@gmail.com>
2021-05-08 15:08:55 +0530

    updates

---
+++ src/homepage.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+import Header from './header';
+const Homepage = () => {
+    return (
+        <div className="main-container">
+            <Header/>
+        </div>
+    );
+}
+
+export default Homepage;
