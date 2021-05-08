92d7810baf6b34a503204947d32dfe87af38b1f4
vikasprashar99 <vikasprashar99@gmail.com>
2021-05-08 15:53:27 +0530

    updates

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -3,7 +3,17 @@ import Header from './header';
 const Homepage = () => {
     return (
         <div className="main-container">
-            <Header/>
+            <Header />
+            <div className="sub-container">
+                <span className="content1">Unlimited movies, TV </span>
+                <span className="content1">shows and more.</span>
+                <span className="content2">Watch anywhere. Cancel anytime.</span><br />
+                <span className="content3">Ready to watch? Enter your email to create or restart your membership.</span>
+            </div><br />
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <input type="text" className="email-text" />
+                <a href="www.google.com" className="get-started-button">Get Started </a>
+            </div>
         </div>
     );
 }
