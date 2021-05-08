bfccb9f9718b657c39f9415a7e9605c4bab869d7
vikasprashar99 <vikasprashar99@gmail.com>
2021-05-08 21:24:09 +0530

    updates

--- src/homepage.tsx
+++ src/homepage.tsx
@@ -3,22 +3,12 @@ import Header from './header';
 const Homepage = () => {
     return (
         <div>
+
             <div className="main-container">
                 <Header />
+
                 <div className="child-container" >
-                    <div style={{ display: "flex", flexDirection: "column" ,textAlign:"center"}}>
-                        <span className="content1">Unlimited movies, TV </span>
-                        <span className="content1">shows and more.</span>
-                        <span className="content2">Watch anywhere. Cancel anytime.</span><br />
-                        <span className="content3">Ready to watch? Enter your email to create or restart your membership.</span>
-                    </div><br/>
-                    <div style={{ display: "flex", justifyContent: "center", top: "50%" }}>
-                        <input type="text" className="email-text" />
-                        <a href="www.google.com" className="get-started-button">Get Started </a>
-                    </div>
-                </div>
-                {/* <div className="child-container" >
-                    <div className="sub-container">
+                    <div style={{ display: "flex", flexDirection: "column", textAlign: "center" }}>
                         <span className="content1">Unlimited movies, TV </span>
                         <span className="content1">shows and more.</span>
                         <span className="content2">Watch anywhere. Cancel anytime.</span><br />
@@ -28,7 +18,7 @@ const Homepage = () => {
                         <input type="text" className="email-text" />
                         <a href="www.google.com" className="get-started-button">Get Started </a>
                     </div>
-                </div> */}
+                </div>
             </div>
 
             <div className="child-card-container" >
@@ -39,6 +29,33 @@ const Homepage = () => {
                     <span className="content2">and more.</span>
                 </div>
                 <span className="card-image"></span>
+            </div>
+            <div className="child-card-container" >
+                <span className="card-image"></span>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center center" }}>
+                    <span className="content1">Download your shows</span>
+                    <span className="content2">to watch offline.</span>
+                    <span className="content2">save your favourites easily and always have</span>
+                    <span className="content2">something to watch.</span>
+                </div>
+            </div>
+            <div className="child-card-container" >
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center center" }}>
+                    <span className="content1">Enjoy on your TV.</span>
+                    <span className="content2">Watch on smart TVs, PlayStation, Xbox,</span>
+                    <span className="content2">Chromecast, Apple TV, Blu-ray players and</span>
+                    <span className="content2">and more.</span>
+                </div>
+                <span className="card-image"></span>
+            </div>
+            <div className="child-card-container" >
+                <span className="card-image"></span>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center center" }}>
+                    <span className="content1">Download your shows</span>
+                    <span className="content2">to watch offline.</span>
+                    <span className="content2">save your favourites easily and always have</span>
+                    <span className="content2">something to watch.</span>
+                </div>
             </div>
         </div>
     );
